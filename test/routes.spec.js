f032b4818d1d2519441696fb2c19be81306e9fff
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-14 16:04:18 -0600

    Install express, install mocha, install chai, create test directory and setup route spec

---
+++ test/routes.spec.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../app');
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+
+});
+
+describe('API Routes', () => {
+
+});
