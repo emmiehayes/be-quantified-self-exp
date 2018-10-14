9e93904ab9d087493a8ecade83b9812fd1b2dbed
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-14 16:05:38 -0600

    Test homepage text

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -6,7 +6,16 @@ const server = require('../app');
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-
+  it('should return the homepage with text', done => {
+    chai.request(server)
+      .get('/')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.html;
+        response.res.text.should.equal('Welcome to Quantified Self!');
+        done();
+      });
+  });
 });
 
 describe('API Routes', () => {
