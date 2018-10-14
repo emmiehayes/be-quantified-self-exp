584088bd3dd57167711eeb5447af3431992dade4
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-14 16:11:22 -0600

    Test GET all Foods, add app.GET, add db config to test, test passing

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,6 +3,10 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../app');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
@@ -27,6 +31,40 @@ describe('Client Routes', () => {
   });
 });
 
-describe('API Routes', () => {
+  describe('API Routes', () => {
+    before((done) => {
+      database.migrate.latest()
+        .then(() => done())
+        .catch(error => {
+          throw error;
+        });
+    });
 
+    beforeEach((done) => {
+      database.seed.run()
+        .then(() => done())
+        .catch(error => {
+          throw error;
+        });
+    });
+
+    describe('GET /api/v1/foods', () => {
+      it('should return all foods', done => {
+        chai.request(server)
+          .get('/api/v1/foods')
+          .end((err, response) => {
+            response.should.have.status(200);
+            response.should.be.json;
+            response.body.should.be.a('array');
+            response.body.length.should.equal(5);
+
+            response.body[0].should.have.property('name');
+            response.body[0].name.should.equal('Apple');
+            response.body[0].should.have.property('calories');
+            response.body[0].calories.should.equal('10');
+        
+            done();
+          });
+      });
+    });
 });
