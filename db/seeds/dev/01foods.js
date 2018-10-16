89143d20e74cee1c718d6037d866248f615b74d6
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-15 20:59:46 -0600

    Remove id from food

--- db/seeds/dev/01foods.js
+++ db/seeds/dev/01foods.js
@@ -13,25 +13,25 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         knex('foods').insert({
           id: 1, name: 'Apple', calories: 10
-        }, 'id'),
+        }),
         knex('foods').insert({
           id: 2, name: 'Banana', calories: 20
-        }, 'id'),
+        }),
         knex('foods').insert({
           id: 3, name: 'Orange', calories: 30
-        }, 'id'),
+        }),
          knex('foods').insert({
           id: 4, name: 'Peach', calories: 40
-        }, 'id'),
+        }),
          knex('foods').insert({
           id: 5, name: 'Plum', calories: 50
-        }, 'id'),
+        }),
          knex('foods').insert({
           id: 6, name: 'Pear', calories: 60
-        }, 'id'),
+        }),
          knex('foods').insert({
           id: 7, name: 'Strawberries', calories: 70
-        }, 'id')
+        })
         .then(() => console.log('Seeding Foods complete'))
         .catch(error => console.log(`Error seeding foods data: ${error}`))
       ])
